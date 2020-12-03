fa8166ee32d0563a021c5c52c46c1cd5befa2dda
Bodin Bizetic <dinbo99@gmail.com>
2020-12-03 02:39:21 +0100

    Add buddy_log

--- include/buddy/buddy.h
+++ include/buddy/buddy.h
@@ -2,10 +2,22 @@
 #define _BUDDY_H_
 
 #include <stdlib.h>
+#include "helper.h"
 
-#define BLOCK_SIZE 4096
+#define BLOCK_SIZE 256
 
 void buddy_init(void* vpSpace, size_t size);
+void *buddy_alloc(size_t size);
 
+void buddy_print_memory_offsets();
+
+
+#if defined(LOGING) && defined(LOGING_BUDDY)
+
+#define BUDDY_LOG(str, ...) LOG(str, __VA_ARGS__)
+
+#else
+#define BUDDY_LOG(str, ...)
+#endif // BUDDY_LOG
 
 #endif
