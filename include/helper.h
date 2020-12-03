6f084768f8db36c935068f2614649167a52ccbc0
Bodin Bizetic <dinbo99@gmail.com>
2020-12-03 02:40:48 +0100

    Add assertion, types and bit helpers

--- include/helper.h
+++ include/helper.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
+//******************************************************************//
+// LOGING *******************************************************//
+
 #define LOGING
 #define LOGING_BUDDY
 
@@ -17,20 +20,28 @@
 
 #endif // LOGING
 
+//******************************************************************//
+// BIT HELPER *******************************************************//
 
-#if defined(LOGING) && defined(LOGING_BUDDY)
+#define FLS(num) (num ? sizeof(num) * CHAR_BIT - __builtin_clz(num) : 0)
 
-#define BUDDY_LOG(str, ...) LOG(str, __VA_ARGS__)
-
-#else
-#define BUDDY_LOG(str, ...)
-#endif // BUDDY_LOG
-
-
-#define FLS(num) sizeof(num) * CHAR_BIT - __builtin_clz(num)
-
-#define BEST_FIT_BLOCKID(num) FLS(num-1) + 1
+// Find first >= POW_TWO
+#define BEST_FIT_BLOCKID(num) FLS(num-1)
 
 #define POWER_OF_TWO(num) ((num & (num - 1)) == 0)
-#define ROUND_TO_POWER_OF_TWO(num) (1  << (BEST_FIT_BLOCKID(num)-1))
+#define ROUND_TO_POWER_OF_TWO(num) (1  << (BEST_FIT_BLOCKID(num)))
+
+//******************************************************************//
+// ASSERTION *******************************************************//
+#include <assert.h>
+
+#define ASSERT(exp) assert(exp)
+
+//******************************************************************//
+// CUSTOM TYPES *******************************************************//
+
+typedef char bool;
+#define true 1
+#define false 0
+
 #endif // __HELPER_H
