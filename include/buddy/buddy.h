c60ea93e079f60f0d936edd16ff7694bd644ab57
Bodin Bizetic <dinbo99@gmail.com>
2020-12-10 02:31:54 +0100

    Move buddy structure to header

--- include/buddy/buddy.h
+++ include/buddy/buddy.h
@@ -2,12 +2,31 @@
 #define _BUDDY_H_
 
 #include "helper.h"
+#include <stdint.h>
 #include <stdlib.h>
 
 #define BLOCK_SIZE 256
 
+typedef struct buddy_block_struct
+{
+    struct buddy_block_struct *prev;
+    struct buddy_block_struct *next;
+    uint8_t blockid;
+} buddy_block_t;
+
+typedef struct buddy_allocator_struct
+{
+    void *vpStart;
+    size_t totalSize;
+    uint8_t maxBlockSize;
+    void *vpMemoryStart;
+    size_t memorySize;
+    buddy_block_t **vpMemoryBlocks;
+} buddy_allocator_t;
+
 void buddy_init(void *vpSpace, size_t size);
 void *buddy_alloc(size_t size);
+void buddy_free(void *ptr, size_t size);
 
 void buddy_print_memory_offsets();
 
